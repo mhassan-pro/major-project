80d06adae30a275ac0e36828cf24911f0890ac83
mhassan-pro <mhassan-pro@users.noreply.github.com>
2025-11-13 21:11:07 +0500

    Add wrapAsync utility and enhance form validation in edit listing view; include feedback messages for improved user experience

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require('ejs-mate');
+const wrapAsync = require("./utils/wrapAsync.js");
 
 
 app.use(methodOverride("_method"));
@@ -46,12 +47,14 @@ app.get("/listings/:id",async (req,res) => {
     res.render("listings/show.ejs",{listing});
 })
 // create route
-app.post("/listings",async (req,res) => {
+app.post("/listings",
+    wrapAsync (async (req,res) => {
    // let {title,description,price,location,country} = req.body;
-   let listing = new Listing(req.body.listing);
-   await listing.save();
-    res.redirect("/listings");
-})
+        let listing = new Listing(req.body.listing);
+        await listing.save();
+        res.redirect("/listings");
+    })
+)
 
 //edit route
 app.get("/listings/:id/edit",async (req,res) => {
@@ -80,6 +83,11 @@ app.get("/",(req,res)=>{
     console.log("Home route accessed");
     res.send("Welcome to Wanderlust Home Page");
 })
+
+app.use((err,req,res,next)=>{
+    res.send("Something went wrong");
+})
+
 app.listen(8080,()=>{
     console.log("Server is running on port 8080");
 });
